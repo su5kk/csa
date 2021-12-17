89a4d332974b3515c24c5dd2b057f45bf1476e78
su5kk <temiki36@gmail.com>
2021-12-17 23:53:17 +0300

    [hw5] added for loop.

--- homework/implementation/code/main.cpp
+++ homework/implementation/code/main.cpp
@@ -11,37 +11,51 @@ void process(Cipher cipher, std::string msg, std::promise<string> promise) {
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc < 2)
     {
-        std::cout << "task5 msg_chunk1 msg_chunk2 msh_chunk3" << '\n';
+        std::cout << "task5 <n> <s1> ... <sn>" << '\n';
         std::cout << "message chunks are the whole message divided into 3 parts." << '\n';
         return 1;
     }
-    std::string msg1 = argv[1];
-    std::cout << "first chunk: " << msg1 << '\n';
-    std::string msg2 = argv[2];
-    std::cout << "second chunk: " << msg2 << '\n';
-    std::string msg3 = argv[3];
-    std::cout << "third chunk: " << msg3 << '\n';
-
-    std::promise<string> out_msg1;
-    std::promise<string> out_msg2;
-    std::promise<string> out_msg3;
-
-    Cipher* cipher = new Cipher();
-    auto out1 = out_msg1.get_future();
-    auto out2 = out_msg2.get_future();
-    auto out3 = out_msg3.get_future();
-
-    std::thread f1(&process, *cipher, msg1, std::move(out_msg1));
-    std::thread f2(&process, *cipher, msg2, std::move(out_msg2));
-    std::thread f3(&process, *cipher, msg3, std::move(out_msg3));
-
-    f1.join();
-    f2.join();
-    f3.join();
-
-    std::cout << out1.get() << '\n';
-    std::cout << out2.get() << '\n';
-    std::cout << out3.get() << '\n';
+    int n = std::atoi(argv[1]);
+    std::cout << "n is: " << n << '\n';
+    if (argc - 2 != n) {
+        std::cout << "not enough strings" << '\n';
+        return 1;
+    }
+    auto cipher = new Cipher();
+    for (int i = 0; i < n; ++i) {
+        std::string msg = argv[i + 2];
+        std::cout << "chunk is: " << msg << '\n';
+        std::promise<string> out_msg;
+        auto out = out_msg.get_future();
+        std::thread a(&process, *cipher, msg, std::move(out_msg));
+        a.join();
+        std::cout << "cipher is: " << out.get() << '\n';
+    }
+//
+//    std::string msg1 = argv[1];
+//    std::string msg2 = argv[2];
+//    std::string msg3 = argv[3];
+//
+//    std::promise<string> out_msg1;
+//    std::promise<string> out_msg2;
+//    std::promise<string> out_msg3;
+//
+//    Cipher* cipher = new Cipher();
+//    auto out1 = out_msg1.get_future();
+//    auto out2 = out_msg2.get_future();
+//    auto out3 = out_msg3.get_future();
+//
+//    std::thread f1(&process, *cipher, msg1, std::move(out_msg1));
+//    std::thread f2(&process, *cipher, msg2, std::move(out_msg2));
+//    std::thread f3(&process, *cipher, msg3, std::move(out_msg3));
+//
+//    f1.join();
+//    f2.join();
+//    f3.join();
+//
+//    std::cout << out1.get() << '\n';
+//    std::cout << out2.get() << '\n';
+//    std::cout << out3.get() << '\n';
 }
