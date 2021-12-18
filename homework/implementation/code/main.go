dde0305d110dff4fe42303d59cc94571cce74ebe
su5kk <temiki36@gmail.com>
2021-12-18 13:16:54 +0300

    [hw5-go] refactor.

--- homework/implementation/code/main.go
+++ homework/implementation/code/main.go
@@ -8,12 +8,16 @@ import (
 	"time"
 )
 
-type Cipher struct {
+type Encryptor interface {
+	Encrypt(msg string, out chan string, wg *sync.WaitGroup)
+}
+
+type SimpleCipher struct {
 	table map[rune]int
 }
 
-func New() *Cipher {
-	cipher := &Cipher{
+func New() *SimpleCipher {
+	cipher := &SimpleCipher{
 		table: make(map[rune]int),
 	}
 	for i := 0; i < 26; i++ {
@@ -22,7 +26,7 @@ func New() *Cipher {
 	return cipher
 }
 
-func (c *Cipher) Encrypt(msg string, out chan string, wg *sync.WaitGroup) {
+func (c *SimpleCipher) Encrypt(msg string, out chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	encryptedMessage := ""
 	for _, v := range msg {
@@ -32,15 +36,10 @@ func (c *Cipher) Encrypt(msg string, out chan string, wg *sync.WaitGroup) {
 	out <- encryptedMessage
 }
 
-func main() {
-	start := time.Now()
-	cipher := New()
-	strings := os.Args[1:]
-	receiver := make(chan string)
-	wg := sync.WaitGroup{}
-	for _, str := range strings {
+func run(wg *sync.WaitGroup, messages []string, receiver chan string, cipher Encryptor) {
+	for _, str := range messages {
 		wg.Add(1)
-		go cipher.Encrypt(str, receiver, &wg)
+		go cipher.Encrypt(str, receiver, wg)
 	}
 	go func() {
 		wg.Wait()
@@ -49,5 +48,14 @@ func main() {
 	for ans := range receiver {
 		fmt.Println(ans)
 	}
+}
+
+func main() {
+	start := time.Now()
+	cipher := New()
+	strings := os.Args[1:]
+	receiver := make(chan string)
+	wg := sync.WaitGroup{}
+	run(&wg, strings, receiver, cipher)
 	fmt.Println(time.Since(start).String())
 }
